c7fb88e0a0f962453917d776e1a6244d6fa78b7e
Erki Suurjaak <erki@lap.ee>
2020-06-17 18:13:47 +0300

    Added 64-bit version of sqlite3_analyzer for Linux.

--- sqlitely/conf.py
+++ sqlitely/conf.py
@@ -17,12 +17,13 @@ from ConfigParser import RawConfigParser
 import datetime
 import json
 import os
+import platform
 import sys
 
 
 """Program title, version number and version date."""
 Title = "SQLitely"
-Version = "1.0.dev301"
+Version = "1.0.dev302"
 VersionDate = "17.06.2020"
 
 if getattr(sys, "frozen", False):
@@ -149,9 +150,11 @@ DBsOpen = {}
 
 """Path to SQLite analyzer tool."""
 DBAnalyzer = os.path.join(BinDirectory, "sqlite3_analyzer" + (
-    ".exe" if "win32"  == sys.platform else
-    "_osx" if "darwin" == sys.platform else "_linux"
+    ".exe"   if "win32"  == sys.platform else
+    "_osx"   if "darwin" == sys.platform else
+    "_linux" if "64" not in platform.architecture()[0] else "_linux_x64"
 ))
+
 
 """Whether logging to log window is enabled."""
 LogEnabled = True
