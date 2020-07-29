b1dc792792a6b18c0dac45fe5889d999eaae26a8
Erki Suurjaak <erki@lap.ee>
2020-07-29 21:23:49 +0300

    Made database detection thread respond faster to canceling.

--- sqlitely/conf.py
+++ sqlitely/conf.py
@@ -10,7 +10,7 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     21.08.2019
-@modified    28.07.2020
+@modified    29.07.2020
 ------------------------------------------------------------------------------
 """
 from ConfigParser import RawConfigParser
@@ -23,8 +23,8 @@ import sys
 
 """Program title, version number and version date."""
 Title = "SQLitely"
-Version = "1.1.dev2"
-VersionDate = "28.07.2020"
+Version = "1.1.dev3"
+VersionDate = "29.07.2020"
 
 if getattr(sys, "frozen", False):
     # Running as a pyinstaller executable
