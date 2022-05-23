c21d4285fd09b349d1cd22b50172df279a1c06b0
Erki Suurjaak <erki@lap.ee>
2022-05-23 19:29:40 +0300

    Update setup.py to retain image links and point local repository links to GitHub.

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ Released under the MIT License.
 
 @author      Erki Suurjaak
 @created     21.08.2019
-@modified    27.03.2022
+@modified    23.05.2022
 ------------------------------------------------------------------------------
 """
 import os
 import re
 import sys
+
 import setuptools
 
 ROOTPATH  = os.path.abspath(os.path.dirname(__file__))
@@ -21,47 +22,52 @@ sys.path.insert(0, os.path.join(ROOTPATH, "src"))
 
 from sqlitely import conf
 
+
 PACKAGE = conf.Title.lower()
+REPOSITORY = "https://github.com/suurjaak/SQLitely"
 
 
 def readfile(path):
     """Returns contents of path, relative to current file."""
     root = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(root, path)) as f: s = f.read().decode("utf-8")
-    md_link_rgx = r"\[(.+)\]\((.+)+\)"
-    def repl(m):
-        m2 = re.search(md_link_rgx, m.group(1))
-        if m2: return "%s: %s" % (m2.group(1), m2.group(2)) # Image link
-        elif m.group(1) == m.group(2): return m.group(1)    # Local link
-        else: return "%s (%s)" % (m.group(1), m.group(2)) # Web link
-    s = re.sub(md_link_rgx, repl, s)
-    return s
+    with open(os.path.join(root, path)) as f: return f.read()
+
+def get_description():
+    """Returns package description from README."""
+    LINK_RGX = r"\[([^\]]+)\]\(([^\)]+)\)"  # 1: content in [], 2: content in ()
+    KEEP = ("ftp://", "http://", "https://", "www.")
+    # Unwrap page anchor links like [Page link](#page-link) as "Page link",
+    # make package file links like [LICENSE.md](LICENSE.md) point to repository
+    repl = lambda m: m.group(1) if m.group(2).startswith("#") else \
+                     m.group(0) if any(map(m.group(2).startswith, KEEP)) else \
+                     "[%s](%s/blob/master/%s)" % (m.group(1), REPOSITORY, m.group(2))
+    return re.sub(LINK_RGX, repl, readfile("README.md"))
 
 
 setuptools.setup(
-    name=PACKAGE,
-    version=conf.Version,
-    description="SQLite database tool",
-    url="https://github.com/suurjaak/SQLitely",
+    name                 = PACKAGE,
+    version              = conf.Version,
+    description          = "SQLite database tool",
+    url                  = REPOSITORY,
 
-    author="Erki Suurjaak",
-    author_email="erki@lap.ee",
-    license="MIT",
-    platforms=["any"],
-    keywords="sqlite database",
+    author               = "Erki Suurjaak",
+    author_email         = "erki@lap.ee",
+    license              = "MIT",
+    platforms            = ["any"],
+    keywords             = "sqlite database",
 
-    install_requires=["appdirs", "openpyxl", "Pillow", "pyparsing", "pytz", "six",
-                      "wxPython>=4.0", "xlrd", "XlsxWriter"],
-    extras_require={
+    install_requires     = ["appdirs", "openpyxl", "Pillow", "pyparsing", "pytz", "six",
+                            "wxPython>=4.0", "xlrd", "XlsxWriter"],
+    extras_require       = {
         ':python_version < "3"': ["antlr4-python2-runtime==4.9"],
         ':python_version > "3"': ["antlr4-python3-runtime==4.9"],
     },
-    entry_points={"gui_scripts": ["{0} = {0}.main:run".format(PACKAGE)]},
+    entry_points         = {"gui_scripts": ["{0} = {0}.main:run".format(PACKAGE)]},
 
-    package_dir={"": "src"},
-    packages=[PACKAGE],
-    include_package_data=True, # Use MANIFEST.in for data files
-    classifiers=[
+    package_dir          = {"": "src"},
+    packages             = [PACKAGE],
+    include_package_data = True, # Use MANIFEST.in for data files
+    classifiers          = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: End Users/Desktop",
         "Operating System :: Microsoft :: Windows",
@@ -76,6 +82,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
     ],
 
-    long_description_content_type="text/markdown",
-    long_description=readfile("README.md"),
+    long_description_content_type = "text/markdown",
+    long_description = get_description(),
 )
