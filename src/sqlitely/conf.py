f40d7f0bb5a3f7c973b852b614a6791e727e98d7
Erki Suurjaak <erki@lap.ee>
2023-05-25 20:16:46 +0300

    Update SQLite reserved keywords list.

--- src/sqlitely/conf.py
+++ src/sqlitely/conf.py
@@ -29,7 +29,7 @@ except ImportError: wx = None
 
 """Program title, version number and version date."""
 Title = "SQLitely"
-Version = "2.1.dev91"
+Version = "2.1.dev92"
 VersionDate = "25.05.2023"
 
 if getattr(sys, "frozen", False):
