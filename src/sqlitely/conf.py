6bc1bdb8d0b557d816b00872d63d5663dc4050cb
Erki Suurjaak <erki@lap.ee>
2024-12-04 21:24:45 +0200

    Reorder configuration directives by category and alphabet.

--- src/sqlitely/conf.py
+++ src/sqlitely/conf.py
@@ -32,7 +32,7 @@ except ImportError: wx = None
 
 """Program title, version number and version date."""
 Title = "SQLitely"
-Version = "2.4.dev218"
+Version = "2.4.dev219"
 VersionDate = "04.12.2024"
 
 Frozen, Snapped = getattr(sys, "frozen", False), (sys.executable or "").startswith("/snap/")
@@ -76,35 +76,35 @@ OptionalFileDirectives = [
 ]
 Defaults = {}
 
-"""---------------------------- FileDirectives: ----------------------------"""
+"""---------------------------- File directives: ----------------------------"""
 
-"""All detected/added databases."""
-DBFiles = []
+"""Whether program can have multiple instances running, or reuses one instance."""
+AllowMultipleInstances = False
+
+"""History of commands entered in console."""
+ConsoleHistoryCommands = []
 
 """Database filename extensions, as ('.extension', )."""
 DBExtensions = [".db", ".db3", ".s3db", ".sl3", ".sqlite", ".sqlite3", ".sqlitedb"]
 
+"""All detected/added databases."""
+DBFiles = []
+
 """Database list sort state, [col, ascending]."""
 DBSort = []
 
-"""Whether program can have multiple instances running, or reuses one instance."""
-AllowMultipleInstances = False
+"""History of search/replace texts in find/replace dialogs, as {"find": [..], "replace": [..]}."""
+FindReplaceHistory = {}
+
+"""Identifier for inter-process communication."""
+IPCName = quote_plus("%s-%s" % (wx.GetUserId(), ApplicationFile)).encode("latin1", "replace") \
+          if wx else ""
 
 """
 Port for inter-process communication, receiving data from other
 launched instances if not AllowMultipleInstances.
 """
 IPCPort = 59987
-
-"""Identifier for inter-process communication."""
-IPCName = quote_plus("%s-%s" % (wx.GetUserId(), ApplicationFile)).encode("latin1", "replace") \
-          if wx else ""
-
-"""History of commands entered in console."""
-ConsoleHistoryCommands = []
-
-"""History of search/replace texts in find/replace dialogs, as {"find": [..], "replace": [..]}."""
-FindReplaceHistory = {}
 
 """Index of last active page in database tab, {db path: index}."""
 LastActivePages = {}
@@ -118,20 +118,38 @@ LastSearchResults = {}
 """Files selected in the database lists on last run."""
 LastSelectedFiles = []
 
+"""Date string of last time updates were checked."""
+LastUpdateCheck = None
+
+"""Whether to log all SQL statements to log window."""
+LogSQL = False
+
+"""Maximum number of console history commands to store."""
+MaxConsoleHistory = 1000
+
 """Maximum database file size for doing full COUNT(*) instead of estimating from MAX(ROWID)."""
 MaxDBSizeForFullCount = 500000000
-
-"""Maximum table ROWID for doing full COUNT(*) if database size over MaxDBSizeForFullCount."""
-MaxTableRowIDForFullCount = 1000
 
 """Maximum import file size to do full row count for."""
 MaxImportFilesizeForCount = 10 * 1e6
 
-"""Number of rows to seek ahead on data grids, when scrolling to end of retrieved rows."""
-SeekLength = 100
+"""Maximum number of cached SQL parse results."""
+MaxParseCache = 500
 
-"""Number of rows to seek ahead on data grids, when scrolling freely or jumping to data grid bottom."""
-SeekLeapLength = 10000
+"""How many items in the Recent Files menu."""
+MaxRecentFiles = 20
+
+"""Maximum number of search texts to store."""
+MaxSearchHistory = 500
+
+"""Maximum number of results to show in search results."""
+MaxSearchResults = 500
+
+"""Maximum table ROWID for doing full COUNT(*) if database size over MaxDBSizeForFullCount."""
+MaxTableRowIDForFullCount = 1000
+
+"""Minimum allowed size for the main window, as (width, height)."""
+MinWindowSize = (600, 400)
 
 """Cached parse results, as {CREATE SQL: {meta}}."""
 ParseCache = {}
@@ -143,6 +161,9 @@ E.g. {"ValueEditorFunctions": [{title, body, name, ?active}]} for column value e
 """
 Plugins = {}
 
+"""Whether to pop up message dialogs for unhandled errors."""
+PopupUnexpectedErrors = True
+
 """Contents of Recent Files menu."""
 RecentFiles = []
 
@@ -152,11 +173,14 @@ RunChecksums = True
 """Run statistics analysis automatically (may take a while for large databases)."""
 RunStatistics = True
 
+"""Whether database schema diagram is enabled."""
+SchemaDiagramEnabled = True
+
 """Database schema diagram settings, as {path: {..}}."""
 SchemaDiagrams = {}
 
-"""Whether database schema diagram is enabled."""
-SchemaDiagramEnabled = True
+"""Whether to do case-sensitive search."""
+SearchCaseSensitive = False
 
 """
 Texts entered in global search, used for drop down auto-complete.
@@ -164,20 +188,32 @@ Last value can be an empty string: search box had no text.
 """
 SearchHistory = []
 
-"""Whether to create a new tab for each search or reuse current."""
-SearchUseNewTab = True
-
-"""Whether to do case-sensitive search."""
-SearchCaseSensitive = False
+"""Whether to search in all columns of all tables and views."""
+SearchInData = True
 
 """Whether to search in database CREATE SQL."""
 SearchInMeta = False
 
-"""Whether to search in all columns of all tables and views."""
-SearchInData = True
+"""Number of search results to yield in one chunk from search thread."""
+SearchResultsChunk = 50
+
+"""Whether to create a new tab for each search or reuse current."""
+SearchUseNewTab = True
+
+"""Number of rows to seek ahead on data grids, when scrolling freely or jumping to data grid bottom."""
+SeekLeapLength = 10000
+
+"""Number of rows to seek ahead on data grids, when scrolling to end of retrieved rows."""
+SeekLength = 100
 
 """Texts in SQL window, loaded on reopening a database {filename: [(name, text), ], }."""
 SQLWindowTexts = {}
+
+"""Width of the database statistics plots, in pixels."""
+StatisticsPlotWidth = 200
+
+"""Duration of status messages on StatusBar, in seconds."""
+StatusFlashLength = 20
 
 """Show line numbers in SQL controls, like database full schema panel."""
 TextLineNumbers = {}
@@ -191,11 +227,14 @@ TrayIconEnabled = True
 """Whether the program checks for updates every UpdateCheckInterval."""
 UpdateCheckAutomatic = True
 
-"""Whether the program has been minimized and hidden to tray."""
-WindowMinimizedToTray = False
+"""Days between automatic update checks."""
+UpdateCheckInterval = 7
 
 """Whether the program window has been maximized."""
 WindowMaximized = False
+
+"""Whether the program has been minimized and hidden to tray."""
+WindowMinimizedToTray = False
 
 """Main window position, (x, y)."""
 WindowPosition = None
@@ -203,7 +242,7 @@ WindowPosition = None
 """Main window size in pixels, as [w, h]."""
 WindowSize = (1080, 720)
 
-"""---------------------------- /FileDirectives ----------------------------"""
+"""---------------------------- /File directives ----------------------------"""
 
 """Currently opened databases, as {filename: db}."""
 DBsOpen = {}
@@ -219,12 +258,6 @@ DBAnalyzer = os.path.join(BinDirectory, "sqlite3_analyzer" + (
 """Whether logging to log window is enabled."""
 LogEnabled = True
 
-"""Whether to log all SQL statements to log window."""
-LogSQL = False
-
-"""Whether to pop up message dialogs for unhandled errors."""
-PopupUnexpectedErrors = True
-
 """Number of unhandled errors encountered during current runtime."""
 UnexpectedErrorCount = 0
 
@@ -233,35 +266,11 @@ DownloadURL  = "https://erki.lap.ee/downloads/SQLitely/"
 ChangelogURL = "https://suurjaak.github.io/SQLitely/changelog.html"
 HomeUrl      = "https://suurjaak.github.io/SQLitely"
 
-"""Minimum allowed size for the main window, as (width, height)."""
-MinWindowSize = (600, 400)
-
 """Console window size in pixels, (width, height)."""
 ConsoleSize = (800, 300)
 
-"""Maximum number of console history commands to store."""
-MaxConsoleHistory = 1000
-
-"""Maximum number of cached SQL parse results."""
-MaxParseCache = 500
-
-"""Maximum number of search texts to store."""
-MaxSearchHistory = 500
-
-"""Days between automatic update checks."""
-UpdateCheckInterval = 7
-
-"""Date string of last time updates were checked."""
-LastUpdateCheck = None
-
 """Maximum length of a tab title, overflow will be cut on the left."""
 MaxTabTitleLength = 60
-
-"""Maximum number of results to show in search results."""
-MaxSearchResults = 500
-
-"""Number of search results to yield in one chunk from search thread."""
-SearchResultsChunk = 50
 
 """Name of font used in HTML content."""
 HtmlFontName = "Tahoma"
@@ -321,9 +330,6 @@ GridRowInsertedColour = "#B9EAFF"
 """Colour set to table/list cells that have been changed."""
 GridCellChangedColour = "#FFA5A5"
 
-"""Width of the database statistics plots, in pixels."""
-StatisticsPlotWidth = 200
-
 """Colour for tables plot in database statistics."""
 PlotTableColour = "#3399FF"
 
@@ -332,12 +338,6 @@ PlotIndexColour = "#1DAB48"
 
 """Background colour for plots in database statistics."""
 PlotBgColour = "#DDDDDD"
-
-"""Duration of status messages on StatusBar, in seconds."""
-StatusFlashLength = 20
-
-"""How many items in the Recent Files menu."""
-MaxRecentFiles = 20
 
 """Font files used for measuring text extent in export."""
 FontXlsxFile     = os.path.join(ResourceDirectory, "Carlito.ttf")
